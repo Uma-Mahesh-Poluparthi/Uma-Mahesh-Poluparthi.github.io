c1a99f38dcf19f65b2b1b6f047bb968d1cb13920
Uma-Mahesh-Poluparthi <umamahe113@gmail.com>
2025-11-29 19:55:24 +0530

    Update script.js

--- script.js
+++ script.js
@@ -95,3 +95,11 @@ async function loadAchievements() {
 }
 loadAchievements();
 
+function openLightbox(src) {
+    document.getElementById("lightbox-img").src = src;
+    document.getElementById("lightbox").style.display = "flex";
+}
+function closeLightbox() {
+    document.getElementById("lightbox").style.display = "none";
+}
+
