cc42633d00d68adabf37693935bb3961148b1447
Uma-Mahesh-Poluparthi <umamahe113@gmail.com>
2025-11-30 13:04:12 +0530

    Update and rename supabase.js to none1

--- supabase.js
+++
@@ -1,8 +0,0 @@
-// ===============================
-// Supabase Project Config (v2)
-// ===============================
-
-const SUPABASE_URL = "https://xzatttpouvlhqzbuwgmc.supabase.co";
-
-const SUPABASE_ANON_KEY = 
-"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh6YXR0dHBvdXZsaHF6YnV3Z21jIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQ0MDIyMDQsImV4cCI6MjA3OTk3ODIwNH0.b_21iJwV6QrZ87xEVCxZxhwYGqvhBHMbm-W9Chu9RnE";
