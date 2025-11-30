7894c1c8a5362c99fe5187274d6d9b3497f0b663
Uma-Mahesh-Poluparthi <umamahe113@gmail.com>
2025-11-30 11:15:22 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Supabase Setup
 const supabaseUrl = "https://xzatttpouvlhqzbuwgmc.supabase.co";
-const supabaseAnonKey = "YOUR_ANON_KEY_HERE"; // Use your actual key
+const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh6YXR0dHBvdXZsaHF6YnV3Z21jIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQ0MDIyMDQsImV4cCI6MjA3OTk3ODIwNH0.b_21iJwV6QrZ87xEVCxZxhwYGqvhBHMbm-W9Chu9RnE"; // Use your actual key
 const supabaseClient = supabase.createClient(supabaseUrl, supabaseAnonKey);
 
 // Load Gallery
@@ -39,3 +39,4 @@ async function loadGallery() {
 }
 
 loadGallery();
+
